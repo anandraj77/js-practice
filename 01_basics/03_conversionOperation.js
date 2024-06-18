9795bb151e48c7aa4c78525ec7d63c5b42d45739
anandraj77 <anandr1213@gmail.com>
2024-06-18 07:19:37 +0000

    type conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NAN (not a number)
+//  true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true, 0 => true
+// "" => false
+// "anand" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
