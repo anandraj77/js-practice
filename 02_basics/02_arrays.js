8727df658299a7fc660a20e119ca1ea234f477df
anandraj77 <anandr1213@gmail.com>
2024-07-02 16:36:34 +0000

    array functions

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,13 @@
+const marvelHeros = ["thor", "ironman", "spiderman"]
+const dcHeros = ["superman", "flash", "batman"]
+
+marvelHeros.push(dcHeros)
+marvelHeros.push(dcHeros)
+
+console.log(dcHeros);
+console.log(marvelHeros);
+// console.log(marvelHeros[3][1]);
+
+// marvelHeros.concat(dcHeros)
+// console.log(marvelHeros);
+
