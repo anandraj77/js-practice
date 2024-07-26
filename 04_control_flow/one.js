f926e9a8b14fcd6519a43f3b263ebac77de299b5
anandraj77 <anandr1213@gmail.com>
2024-07-26 19:11:07 +0000

    control flows

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -16,9 +16,31 @@ const score = 200
 // }
 // console.log(`User power: ${power}`);
 
-
-const balance = 1000
+// const balance = 1000
 
 // if (balance > 500) console.log("test"), console.log("test2");  -> bad practice
 
 
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+// } else if (balance < 900) {
+//     console.log("less than 900");
+// } else{
+//     console.log("less than 1200");
+// }
+
+
+const userLoggedIn = true
+const debtiCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debtiCard && 2==3) {
+    console.log("allow to buy courses");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
