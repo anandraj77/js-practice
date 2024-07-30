e07de7e67e66e153df5e6b1667c1296190bab6d6
anandraj77 <anandr1213@gmail.com>
2024-07-30 18:20:46 +0000

    iterations

---
+++ 05_iterations/three.js
@@ -0,0 +1,20 @@
+// for of
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const i of arr) {
+    // console.log(i);
+}
+
+const greetings = "hello world!"
+for (const greet of greetings) {
+    // console.log(`Each char is ${greet}`);
+}
+
+// Maps
+
+const map = new Map()
+map.set('IN', )
