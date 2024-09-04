21d631d2e7e60703ee567e32b7f5f48c71c835aa
anandraj77 <anandr1213@gmail.com>
2024-09-04 19:28:57 +0000

    closure

--- 09_classesAndOOP/getter_setter.js
+++ 09_classesAndOOP/getter_setter.js
@@ -4,13 +4,21 @@ class User {
         this.password = password
     }
 
+    get email(){
+        return this._email.toUpperCase()
+    }
+    set email(value){
+        this._email = value
+    }
+
     get password() {
-        return this.password.toUpperCase()
+        return `${this._password}anand`
     }
     set password(value){
-        this.password = this.value
+        this._password = value.toUpperCase()
     }
 }
 
-const anand = new User("anand@google.com", "123")
+const anand = new User("anand@google.com", "abc")
 console.log(anand.password);
+console.log(anand.email);
